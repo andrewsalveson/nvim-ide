b2b0e8a800c180846d256978d1ec836c39aaf392
Andrew Salveson <andrewsalveson@gmail.com>
2018-01-14 23:10:36 -0500

    add install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo apt-get install neovim -y
+sudo apt install ruby ruby-dev python3-pip build-essential cmake libclang-dev libboost-all-dev && pip3 install --user --upgrade neovim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
