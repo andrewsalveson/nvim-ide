f9aeb1770ff8d62c610aea70519fe4d14352865a
root <root@dev-box-2.ngcyc55o0oputmsbcdso3kzlac.bx.internal.cloudapp.net>
2018-01-15 20:46:05 +0000

    add y flag

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install neovim -y
-sudo apt install ruby ruby-dev python3-pip build-essential cmake libclang-dev libboost-all-dev && pip3 install --user --upgrade neovim
+sudo apt install ruby ruby-dev python3-pip build-essential cmake libclang-dev libboost-all-dev -y && pip3 install --user --upgrade neovim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
